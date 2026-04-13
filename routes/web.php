b36161edc536d04886fdbc58f9e634b5d95465ad
webelvina <elvinavalenrira>
2026-04-13 08:56:31 +0700

    cek ulang tugas

--- routes/web.php
+++ routes/web.php
@@ -8,12 +8,7 @@ Route::get('/', function () {
 Route::get('/beranda', function () {
     return view('beranda');
 })->name('beranda');
-Route::get('/home', function () {
-    return view('home');
-})->name('home');
 Route::get('/jurusan', function () {
     return view('jurusan');
 })->name('jusuran');
-Route::get('/berita', function () {
-    return view('berita');
-})->name('berita');
+
